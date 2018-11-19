afd4fe169d431ae737c33ef401ec2f54e8b4866a
George Papa <giorgos.papachristou@commsquare.com>
2018-11-19 17:55:25 +0200

    Add argument parser to run_recommender

--- src/app/run_recommender.py
+++ src/app/run_recommender.py
@@ -2,6 +2,9 @@ from argparse import ArgumentParser
 
 if __name__ == '__main__':
     parser = ArgumentParser()
+
     parser.add_argument(--'input_file_format', type=str, required=True, help = 'Set the input format')
+    parser.add_argument(--'set_model', type=str, required=True, help = 'Set the type of model')
+
     agrs, unknown, = parser.parse_known_args()
     pass
