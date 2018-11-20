1d30249c9e295099ab4854b51d19025c9c5e3314
George Papa <giorgos.papachristou@commsquare.com>
2018-11-20 11:36:36 +0200

    Initialize pre_processor module

---
+++ src/app/pre_processor.py
@@ -0,0 +1,15 @@
+
+class cleaner(object):
+    pass
+
+class Tokenizer(object):
+    pass
+
+class Scaler(object):
+    pass
+
+class Decompositioner(object):
+    pass
+
+class Aggregator(object):
+    pass   
