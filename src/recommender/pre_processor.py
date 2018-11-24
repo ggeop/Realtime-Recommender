aa7eadf433915d398f9531151c1d6ab57872d209
Georgios Papachristou <35366798+ggeop@users.noreply.github.com>
2018-11-24 15:50:08 +0200

    Initialize Cleaner and Vectorizer classes

--- src/recommender/pre_processor.py
+++ src/recommender/pre_processor.py
@@ -1,21 +1,51 @@
+import logging
+from gensim import corpora
+from collections import defaultdict
 
 class Cleaner(object):
-    pass
+    def __init__(self, documents, stop_list=None, lower_case=False, lower_threshold=False):
+        self.documents = documents
+        self.stop_list = stop_list
+        self.lower_case = lower_case
+        self.lower_threshold = lower_threshold
+        logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+    '''
+    Clean function:
+    -Tranform capital to lower
+    -Remove stopwords
+    -Remove words with frequency lower than the threshold
+    '''
+    def clean(self):
+        if self.stop_list and self.lower_case:
+            texts = [[word for word in document.lower().split() if word not in self.stop_list]
+                     for document in self.documents]
+        elif self.stop_list and not self.lower_case:
+            texts = [[word for word in document.split() if word not in self.stop_list]
+                     for document in self.documents]
+        elif not self.stop_list and self.lower_case:
+            texts = [[word for word in document.lower().split()] for document in self.documents]
+        elif not self.stop_list and not self.lower_case:
+            texts = [[word for word in document] for document in self.documents]
+
+        frequency = defaultdict(int)
+        for text in texts:
+            for token in text:
+                frequency[token] += 1
+
+        '''texts format [['..', '..'],['..'],[],...]'''
+        texts = [[token for token in text if frequency[token] > 1]
+                 for text in texts]
 
 
 class Tokenizer(object):
-    pass
+    def __init__(self, texts):
+        self.texts = texts
+        logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-
-class Scaler(object):
-    pass
-
-
-class Decompositioner(object):
-    pass
-
-
-class Aggregator(object):
-    pass   
+    '''Mapping words with unique ids'''
+    def create_dictionary(self):
+        return corpora.Dictionary(self.texts)
+        #TODO: dictionary.save('/tmp/....dict')  # store the dictionary, for future reference
 
 
